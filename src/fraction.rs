11074044e03d569f90948a21e8a85303ea78cd6a
Robert Spencer <general@robertandrewspencer.com>
2020-12-30 13:42:30 +0200

    Touch up fraction code and serialise

--- src/fraction.rs
+++ src/fraction.rs
@@ -1,16 +1,19 @@
-use num::{Zero, Num, Signed};
+use num::{Num, Signed, One, Zero};
+use std::ops::{Add, Sub, Mul, Div, Rem, Neg};
+use std::fmt::{Debug, Display};
 
-use crate::gcd::GCD;
+use crate::gcd::PartialGCD;
 use crate::tex::Tex;
+use crate::serial::Serialisable;
 
 #[derive(Copy, Clone, Debug)]
-pub struct Fraction<T : Copy + Num + GCD + Signed> {
+pub struct Fraction<T : Copy + Num + PartialGCD> {
     num : T,
     den : T
 }
 
 impl<T> Fraction<T>
-where T : Num + GCD + Copy + Signed {
+where T : Num + PartialGCD + Copy + Signed {
     pub fn new(n : T, d : T) -> Fraction<T> {
         if n.is_zero() {
             Fraction {
@@ -20,7 +23,7 @@ where T : Num + GCD + Copy + Signed {
         } else if d.is_negative() {
             Fraction::new(-n, -d)
         } else {
-            let g = n.gcd(&d);
+            let g = n.partial_gcd(&d);
             Fraction {
                 num : n / g,
                 den : d / g,
@@ -38,14 +41,18 @@ where T : Num + GCD + Copy + Signed {
 }
 
 impl<T> PartialEq for Fraction<T>
-where T : Num + GCD + Copy + Signed {
+where T : Num + PartialGCD + Copy + Signed {
     fn eq(&self, other: &Self) -> bool {
         self.num * other.den == self.den * other.num
     }
 }
 
-impl<T> std::ops::Add for Fraction<T>
-where T : Num + GCD + Copy + Signed {
+impl<T> Eq for Fraction<T>
+where T : Num + PartialGCD + Copy + Signed {
+}
+
+impl<T> Add for Fraction<T>
+where T : Num + PartialGCD + Copy + Signed {
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
@@ -56,8 +63,8 @@ where T : Num + GCD + Copy + Signed {
     }
 }
 
-impl<T> std::ops::Sub for Fraction<T>
-where T : Num + GCD + Copy + Signed {
+impl<T> Sub for Fraction<T>
+where T : Num + PartialGCD + Copy + Signed {
     type Output = Self;
 
     fn sub(self, other: Self) -> Self {
@@ -68,39 +75,46 @@ where T : Num + GCD + Copy + Signed {
     }
 }
 
-impl<T> std::ops::Mul for Fraction<T>
-where T : Num + GCD + Copy + Signed {
+impl<T> Mul for Fraction<T>
+where T : Num + PartialGCD + Copy + Signed {
     type Output = Self;
 
     fn mul(self, other: Self) -> Self {
+        let g1 = self.num.partial_gcd(&other.den);
+        let g2 = other.num.partial_gcd(&self.den);
         Fraction::new(
-            self.num * other.num,
-            self.den * other.den
+            (self.num / g1) * (other.num / g2),
+            (self.den / g2) * (other.den / g1)
         )
     }
 }
 
-impl<T> std::ops::Div for Fraction<T>
-where T : Num + GCD + Copy + Signed {
+impl<T> Div for Fraction<T>
+where T : Num + PartialGCD + Copy + Signed {
     type Output = Self;
 
     fn div(self, other: Self) -> Self {
+        let g1 = self.num.partial_gcd(&other.num);
+        let g2 = other.den.partial_gcd(&self.den);
         Fraction::new(
-            self.num * other.den,
-            self.den * other.num
+            (self.num / g1) * (other.den / g2),
+            (self.den / g2) * (other.num / g1)
         )
     }
 }
 
 impl<T> std::convert::From<T> for Fraction<T>
-where T : Num + GCD + Copy + Signed {
+where T : Num + PartialGCD + Copy + Signed {
     fn from(x : T) -> Fraction<T> {
-        Fraction::new(x, T::one())
+        Fraction {
+            num : x,
+            den : T::one()
+        }
     }
 }
 
-impl<T> std::ops::Add<T> for Fraction<T>
-where T : Num + GCD + Copy + Signed {
+impl<T> Add<T> for Fraction<T>
+where T : Num + PartialGCD + Copy + Signed {
     type Output = Self;
 
     fn add(self, other: T) -> Self {
@@ -108,8 +122,8 @@ where T : Num + GCD + Copy + Signed {
     }
 }
 
-impl<T> std::ops::Sub<T> for Fraction<T>
-where T : Num + GCD + Copy + Signed {
+impl<T> Sub<T> for Fraction<T>
+where T : Num + PartialGCD + Copy + Signed {
     type Output = Self;
 
     fn sub(self, other: T) -> Self {
@@ -117,33 +131,33 @@ where T : Num + GCD + Copy + Signed {
     }
 }
 
-impl<T> std::ops::Mul<T> for Fraction<T>
-where T : Num + GCD + Copy + Signed {
+impl<T> Mul<T> for Fraction<T>
+where T : Num + PartialGCD + Copy + Signed {
     type Output = Self;
 
     fn mul(self, other: T) -> Self {
-        self * Fraction::from(other)
+        Fraction::new(self.num * other, self.den)
     }
 }
 
-impl<T> std::ops::Div<T>for Fraction<T>
-where T : Num + GCD + Copy + Signed {
+impl<T> Div<T> for Fraction<T>
+where T : Num + PartialGCD + Copy + Signed {
     type Output = Self;
 
     fn div(self, other: T) -> Self {
-        self / Fraction::from(other)
+        Fraction::new(self.num, self.den * other)
     }
 }
 
-impl<T> num::One for Fraction<T>
-where T : Num + GCD + Copy + Signed {
+impl<T> One for Fraction<T>
+where T : Num + PartialGCD + Copy + Signed {
     fn one() -> Self {
         Fraction::from(T::one())
     }
 }
 
-impl<T> num::Zero for Fraction<T>
-where T : Num + GCD + Copy + Signed {
+impl<T> Zero for Fraction<T>
+where T : Num + PartialGCD + Copy + Signed {
     fn zero() -> Self {
         Fraction::from(T::zero())
     }
@@ -153,8 +167,8 @@ where T : Num + GCD + Copy + Signed {
     }
 }
 
-impl<T> std::ops::Rem for Fraction<T>
-where T : Num + GCD + Copy + Signed {
+impl<T> Rem for Fraction<T>
+where T : Num + PartialGCD + Copy + Signed {
     type Output = Self;
 
     fn rem(self, _other:Self) -> Self {
@@ -162,8 +176,8 @@ where T : Num + GCD + Copy + Signed {
     }
 }
 
-impl<T> std::ops::Neg for Fraction<T>
-where T : Num + GCD + Copy + Signed {
+impl<T> Neg for Fraction<T>
+where T : Num + PartialGCD + Copy + Signed {
     type Output = Self;
 
     fn neg(self) -> Self {
@@ -173,7 +187,7 @@ where T : Num + GCD + Copy + Signed {
 
 
 impl<T> Num for Fraction<T>
-where T : Num + GCD + Copy + Signed {
+where T : Num + PartialGCD + Copy + Signed {
     type FromStrRadixErr = ();
 
     fn from_str_radix(_str: &str, _radix: u32) -> Result<Self, Self::FromStrRadixErr> {
@@ -181,15 +195,15 @@ where T : Num + GCD + Copy + Signed {
     }
 }
 
-impl<T> std::fmt::Display for Fraction<T>
-where T : std::fmt::Display + Num + GCD + Copy + Signed {
+impl<T> Display for Fraction<T>
+where T : Display + Num + PartialGCD + Copy + Signed {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "({})/({})", self.num, self.den)
     }
 }
 
 impl<T> Tex for Fraction<T>
-where T : Copy + Num + GCD + Tex + Signed {
+where T : Copy + Num + PartialGCD + Tex + Signed {
     fn into_tex(&self) -> String {
         if self.den.is_one() {
             self.num.into_tex()
@@ -208,7 +222,7 @@ where T : Copy + Num + GCD + Tex + Signed {
 }
 
 impl<T> Signed for Fraction<T>
-where T : Copy + Num + GCD + Tex + Signed {
+where T : Copy + Num + PartialGCD + Tex + Signed {
     fn is_positive(&self) -> bool {
         self.num.is_positive()
     }
@@ -231,5 +245,44 @@ where T : Copy + Num + GCD + Tex + Signed {
 
     fn signum(&self) -> Fraction<T> {
         unimplemented!()
+    }
+}
+
+impl<T> Serialisable for Fraction<T>
+where T: Serialisable + PartialGCD + Signed + Copy {
+
+    fn serialise(&self) -> String {
+        format!("({})/({})", self.num.serialise(), self.den.serialise())
+    }
+
+    fn deserialise(input : &str) -> Self {
+        let mut parts : Vec<&str> = Vec::new();
+        assert!(input.bytes().nth(0) == Some('(' as u8));
+        let mut i = 0;
+        let mut c = 1;
+        while c > 0 {
+            if input.bytes().nth(i) == None { panic!("Cannot read fraction"); }
+            i += 1;
+            if input.bytes().nth(i) == Some('(' as u8) { c += 1; }
+            if input.bytes().nth(i) == Some(')' as u8) { c -= 1; }
+        }
+        i += 1;
+        assert!(input.bytes().nth(i) == Some('/' as u8));
+        parts.push(&input[1..i-1]);
+        i += 1;
+        assert!(input.bytes().nth(i) == Some('(' as u8));
+        let j = i+1;
+        let mut c = 1;
+        while c > 0 {
+            if input.bytes().nth(i) == None { panic!("Cannot read fraction"); }
+            i += 1;
+            if input.bytes().nth(i) == Some('(' as u8) { c += 1; }
+            if input.bytes().nth(i) == Some(')' as u8) { c -= 1; }
+        }
+        parts.push(&input[j..i]);
+        Fraction::new(
+            T::deserialise(parts[0]),
+            T::deserialise(parts[1]),
+        )
     }
 }
