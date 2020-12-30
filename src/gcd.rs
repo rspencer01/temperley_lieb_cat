0737c953059e6c5bb4d6f4d12401b8807f0cdd24
Robert Spencer <general@robertandrewspencer.com>
2020-12-30 11:15:23 +0200

    Introduce partial GCDs for types where GCD is hard

--- src/gcd.rs
+++ src/gcd.rs
@@ -1,21 +1,37 @@
+/// Types that can calculate greatest common factors.
 pub trait GCD {
+    /// Find a greatest common factor of self and other
     fn gcd(&self, other: &Self) -> Self;
+}
+
+/// Types that can calculate common factors.
+pub trait PartialGCD {
+    /// Find a (large) common factor of self and other
+    fn partial_gcd(&self, other: &Self) -> Self;
+}
+
+impl<T:GCD> PartialGCD for T {
+    #[inline(always)]
+    fn partial_gcd(&self, other: &Self) -> Self {
+        self.gcd(other)
+    }
 }
 
 impl GCD for i128 {
     fn gcd(&self, other: &i128) -> i128 {
-        if *self < *other {
-            other.gcd(self)
-        } else {
-            let mut a = *self;
-            let mut b = *other;
-            while b != 0 {
-                let c = a % b;
-                a = b;
-                b = c;
-            }
-            a
+        let mut a = self.abs();
+        let mut b = other.abs();
+        if a < b {
+            let c = b;
+            b = a;
+            a = c;
         }
+        while b != 0 {
+            let c = a % b;
+            a = b;
+            b = c;
+        }
+        a
     }
 }
 
@@ -31,5 +47,7 @@ mod tests {
         debug_assert_eq!(5.gcd(&19), 1);
         debug_assert_eq!(19.gcd(&19), 19);
         debug_assert_eq!(0.gcd(&17), 17);
+        debug_assert_eq!(1.gcd(&-1), 1);
+        debug_assert_eq!((-15).gcd(&5), 5);
     }
 }
