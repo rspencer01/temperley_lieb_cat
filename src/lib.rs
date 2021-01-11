90d90a66de95753d1fc980216fe6786ff4a7bc79
Robert Spencer <general@robertandrewspencer.com>
2021-01-11 09:58:27 +0200

    Add jones wenzl to public exports

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,13 @@ mod temperley_link;
 mod temperley_diagram;
 mod temperley;
 mod gcd;
+mod jones_wenzl;
 
 pub use temperley_site::Site;
 pub use temperley_link::Link;
 pub use temperley_diagram::TLDiagram;
-pub use temperley::{jw, TLMorphism};
+pub use temperley::TLMorphism;
+pub use jones_wenzl::{jwlp, jw};
 
 pub mod fraction;
 pub mod poly;
