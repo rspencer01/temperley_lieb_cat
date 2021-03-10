0c139752a88899e52d276a69eb2ae17e1f0a626b
Robert Spencer <general@robertandrewspencer.com>
2021-03-10 10:32:43 +0200

    Clean up test

--- tests/two_jw_semisimple_poly.rs
+++ tests/two_jw_semisimple_poly.rs
@@ -4,36 +4,41 @@ use temperley_lieb_cat::fraction::Fraction;
 use temperley_lieb_cat::poly::{quantum, Polynomial};
 use temperley_lieb_cat::{TLMorphism, jw};
 use temperley_lieb_cat::structures::Q;
-use num::Zero;
+use num::{One, Zero};
 
 type R = Fraction<Polynomial<Q>>;
 
 #[test]
 fn two_jw_semisimple_poly() {
-    let n1 : usize = 3;
-    let n2 : usize = 3;
+    let n1 : usize = 2;
+    let n2 : usize = 4;
+    let id = TLMorphism::<R>::id(n1+n2);
     let e : TLMorphism<R> = jw(n1) | jw(n2);
-    let mut phi : Vec<TLMorphism<R>> = Vec::new();
-    for i in 0..n1 as usize+1 {
-        phi.push(
-            e.clone() *
-            TLMorphism::big_u(n1+n2, n1, i) *
-            e.clone() *
-            R::from(quantum(n1) * quantum(n2)));
-    }
+
+    let factor = R::from(quantum(n1) * quantum(n2));
+    let phi = (0..n1+1).map(|i|
+            &(
+            &e *
+            &TLMorphism::big_u(n1+n2, n1, i)) *
+            &e *
+            &factor
+        ).collect::<Vec<_>>();
 
     for k in 0..n1 as usize {
         assert_eq!(
-        phi[k+1].clone() * Fraction::from(quantum(n1-k) * quantum(n2-k)),
-        (phi[1].clone() - TLMorphism::<R>::id(n1+n2) * R::from(quantum(k) * quantum(n1+n2-k+1))) * phi[k].clone());
+        &phi[k+1] * R::from(quantum(n1-k) * quantum(n2-k)),
+        &(phi[1].clone() - &id * R::from(quantum(k) * quantum(n1+n2-k+1))) * &phi[k]);
     }
     assert!(
-        ((phi[1].clone() - TLMorphism::<R>::id(n1+n2) * R::from(quantum(n1) * quantum(n2+1))) * phi[n1].clone()).is_zero()
+        (&(phi[1].clone() - &id * R::from(quantum(n1) * quantum(n2+1))) * &phi[n1]).is_zero()
     );
 
-    let mut t = TLMorphism::id((n1+n2) as usize);
+    let mut min_poly = Polynomial::<R>::one();
     for i in 0..n1+1 {
-        t = t * (phi[1].clone() - TLMorphism::<R>::id((n1+n2) as usize) * R::from(quantum(i) * quantum(n1+n2-i +1)));
+        min_poly = min_poly * (Polynomial::gen() - R::from(quantum(i) * quantum(n1+n2-i+1)));
     }
-    assert!(t.is_zero());
+    assert!(
+        min_poly.eval(phi[1].clone(), id).is_zero()
+    );
+
 }
