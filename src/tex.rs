46b66c9a4d9c7b4aaa1317791c94ba7fa18c59c8
Robert Spencer <general@robertandrewspencer.com>
2021-03-11 12:21:58 +0200

    Document TeX better and use which to find the latex

--- src/tex.rs
+++ src/tex.rs
@@ -1,11 +1,27 @@
+//! Traits for writing out TeX files
+
 use std::io::Write;
 use std::process::{Command, Stdio};
+extern crate which;
+use which::which;
 
+/// A trait to render the type into (La)TeX
 pub trait Tex {
     fn into_tex(&self) -> String;
 
+    /// Indicate if this element should be surrounded by braces to eliminate ambiguity
+    ///
+    /// This should be true for items separated by `+`, such as `x + y`, but false
+    /// if there is no ambiguity.  The function `Tex::into_tex` should use this
+    /// to determine if sub-expressions need to be places in parenthesis.
     fn is_multiterm(&self) -> bool;
 
+    /// Renders this object to a PDF file for displaying
+    ///
+    /// The output is written to the file `texput.pdf` in the current directory.
+    ///
+    /// This can be very useful if your PDF viewer automatically updates its display
+    /// when the underlying file changes.
     fn render_tex(&self) {
         let document = format!(
     "\\documentclass[crop=true]{{standalone}}
@@ -20,10 +36,9 @@ pub trait Tex {
     $
 
     \\end{{document}}", self.into_tex());
-        let mut command = Command::new("/home/robert/bin/pdflatex")
+        let mut command = Command::new(which("pdflatex").expect("Could not find latex compiler"))
             .stdin(Stdio::piped())
             .stdout(Stdio::null())
-            .arg("-output-directory=/tmp")
             .arg("-shell-escape")
             .spawn()
             .unwrap();
